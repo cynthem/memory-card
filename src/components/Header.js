55ab46221bf8b1d65183ab753aced0f284e45891
doozles411 <cindy.phelps01@gmail.com>
2022-09-15 19:01:36 -0700

    Import title image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import title from '../assets/title.png';
 
 const Header = () => {
     return (
         <div className='header'>
             <img
             className="title-img"
-            alt="Confundo title"
-            src="../assets/title.png"
+            alt="Confundo"
+            src={title}
             />
         </div>
     )
