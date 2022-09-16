bf6b93e4bf61f528e2c1fd30a926615e8692bdb4
doozles411 <cindy.phelps01@gmail.com>
2022-09-16 11:51:51 -0700

    Insert broom images

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import title from '../assets/title.png';
+import broomLeft from '../assets/broomLeft.png';
+import broomRight from '../assets/broomRight.png';
 
 const Header = () => {
     return (
         <div className="header">
             <img
+            className="broom-left"
+            alt="Broom"
+            src={broomLeft}
+            />
+            <img
             className="title-img"
             alt="Confundo"
             src={title}
+            />
+            <img
+            className="broom-right"
+            alt="Broom"
+            src={broomRight}
             />
         </div>
     )
