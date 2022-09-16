2e2ee096f122385757d2c9b2f64625af4dd4fed1
doozles411 <cindy.phelps01@gmail.com>
2022-09-16 14:27:29 -0700

    Change scores layout

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -3,8 +3,14 @@ import React from 'react';
 const Scores = () => {
     return (
         <div className="scores">
-            <div className="current-score">Current score:</div>
-            <div className="best-score">Best score:</div>
+            <div className="current-score">
+                <p className='current-top'>Current score:</p>
+                <p className='current-bottom'>12</p>
+            </div>
+            <div className="best-score">
+                <p className='best-top'>Best score:</p>
+                <p className='best-bottom'>1</p>
+            </div>
         </div>
     )
 }
