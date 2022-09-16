308450e8199466b5d0501bba8f18ebc57a841ae3
doozles411 <cindy.phelps01@gmail.com>
2022-09-16 12:54:27 -0700

    Fix class name typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className="footer">
             <p className="footer-text">Made by CMP</p>
-            <a classname="footer-link" href="https://github.com/doozles411">
+            <a className="footer-link" href="https://github.com/doozles411">
                 <img
                 className="footer-img"
                 alt="Golden snitch"
