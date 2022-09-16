58592b8511963d1a0c474c23d6e65d96afe98e57
doozles411 <cindy.phelps01@gmail.com>
2022-09-16 14:34:51 -0700

    Insert card image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import dumbledore from '../assets/dumbledore.jpg';
 
 const Card = () => {
     return (
         <div className="card">
-            
+            <img
+            className="card-img"
+            alt="Dumbledore"
+            src={dumbledore}
+            />
         </div>
     )
 }
