6756cb41d612f86fe9a000730400f1afe8265f67
doozles411 <cindy.phelps01@gmail.com>
2022-09-15 19:00:00 -0700

    Import Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Header = () => {
     return (
         <div className='header'>
-            
+            <img
+            className="title-img"
+            alt="Confundo title"
+            src="../assets/title.png"
+            />
         </div>
     )
 }
