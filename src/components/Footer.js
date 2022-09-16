e0e5a0229293d434dbbc9b4f8371e996e81352dd
doozles411 <cindy.phelps01@gmail.com>
2022-09-16 12:46:49 -0700

    Upload image

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p className="footer-text">Made by CMP</p>
+            <img
+            className="footer-img"
+            alt="Broom"
+            src={broomRight}
+            />
+        </div>
+    )
+}
+
+export default Footer;
