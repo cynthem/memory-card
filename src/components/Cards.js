e886ebfb110172930d791847d0041b30e3151f4e
doozles411 <cindy.phelps01@gmail.com>
2022-09-16 17:26:38 -0700

    Create state and methods

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -13,7 +13,74 @@ import ron from '../assets/ron';
 import sirius from '../assets/sirius';
 import voldemort from '../assets/voldemort';
 
-const Cards = () => {
+const Cards = (props) => {
+    const { score, bestScore, handleGame } = props;
+
+    let characters = [
+        {
+            src: dolores,
+            id: 'dolores'
+        },
+        {
+            src: hagrid,
+            id: 'hagrid'
+        },
+        {
+            src: dumbledore,
+            id: 'dumbledore'
+        },
+        {
+            src: ginny,
+            id: 'ginny'
+        },
+        {
+            src: harry,
+            id: 'harry'
+        },
+        {
+            src: hermione,
+            id: 'hermione'
+        },
+        {
+            src: luna,
+            id: 'luna'
+        },
+        {
+            src: malfoy,
+            id: 'malfoy'
+        },
+        {
+            src: mcgonagall,
+            id: 'mcgonagall'
+        },
+        {
+            src: ron,
+            id: 'ron'
+        },
+        {
+            src: sirius,
+            id: 'sirius'
+        }, 
+        {
+            src: voldemort,
+            id: 'voldemort'
+        }
+    ];
+
+    const [cards, setCards] = useState(characters);
+
+    const shuffleCards = (newDeck) => {
+        for (let i = newDeck.length - 1; i > 0; i--) {
+            let random = Math.floor(Math.random() * i);
+            [newDeck[random], newDeck[i]] = [newDeck[i], newDeck[random]];
+        }
+    }
+
+    useEffect(() => {
+        const newDeck = [...cards];
+        shuffleCards(newDeck);
+        setCards(newDeck);
+    }, [score, bestScore]);
 
     return (
         <div className="cards-grid">
