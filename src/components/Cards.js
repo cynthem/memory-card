0fbb69871ef219e78c97b373ab2a2a34fd8d04d1
doozles411 <cindy.phelps01@gmail.com>
2022-09-16 17:15:43 -0700

    Import images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
+import dolores from '../assets/dolores';
+import hagrid from '../assets/hagrid';
+import dumbledore from '../assets/dumbledore';
+import ginny from '../assets/ginny';
+import harry from '../assets/harry';
+import hermione from '../assets/hermione';
+import luna from '../assets/luna';
+import malfoy from '../assets/malfoy';
+import mcgonagall from '../assets/mcgonagall';
+import ron from '../assets/ron';
+import sirius from '../assets/sirius';
+import voldemort from '../assets/voldemort';
 
-const Cards = (/*{ characters }*/) => {
-    /*const characterCards = characters.map((character) => (
-        <Card key={character.id} />
-    ));
-
-    return (
-        <div className="cards">
-            {characterCards}
-        </div>
-    )*/
+const Cards = () => {
 
     return (
         <div className="cards-grid">
--- src/shuffle.js
+++
@@ -1,3 +0,0 @@
-export const shuffle = (arr) => {
-    return [...arr].sort(() =>  Math.random() - 0.5);
-}
