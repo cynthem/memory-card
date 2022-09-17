74f0eb662e1c753c400f62e045f0a34a395746e3
doozles411 <cindy.phelps01@gmail.com>
2022-09-16 17:10:37 -0700

    Pass props to children

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Scores from './Scores';
 import Cards from './Cards';
 
 const Main = () => {
-    const [characters, setCharacters] = useState([]);
+    const [score, setScore] = useState(0);
+    const [bestScore, setBestScore] = useState(0);
+    const [cards, setCards] = useState([]);
+
+    const handleScore = () => {
+        setScore((prevScore) => prevScore + 1);
+    }
+
+    const handleBestScore = () => {
+        setBestScore((score));
+    }
+
+    const handleCard = (characterName) => {
+        setCards((prevArr) => [...prevArr, characterName]);
+    }
+
+    const resetGame = () => {
+        setScore(0);
+        setCards([]);
+    }
+
+    const handleGame = (characterName) => {
+        if (!cards.includes(characterName)) {
+            handleCard(characterName);
+            handleScore();
+        } else {
+            handleBestScore();
+            resetGame();
+        }
+    }
 
     return (
         <div className="main">
-            <Scores />
-            <Cards /*characters={characters}*/ />
+            <Scores score={score} bestScore={bestScore} />
+            <Cards 
+                handleGame={handleGame} 
+                score={score}
+                bestScore={bestScore}
+            />
         </div>
     )
 }
